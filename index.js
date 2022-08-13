e3dc379dd5bb6f11a9f32f4eaccd02e82b31fca2
Joseph Archer <viewtiful.kja@gmail.com>
2022-08-13 16:52:33 -0400

    Created api path for characters. Established NPM requirements

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+// Instantiate package requirements
+const PORT = 3001 || process.env.PORT;
 const express = require('express');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+// Call express function for usage
+const app = express();
+const characters = []
+
+app.get('/', function (req, res) {
+    res.send("Welcome to the Wonderful World of One Piece!")
+
+})
+
+const test = "zoro"
+app.get('/bio', (req, res) => {
+    axios.get('https://onepiece.fandom.com/wiki/Straw_Hat_Pirates')
+        .then((response) => {
+            const html = response.data;
+            const $ = cheerio.load(html)
+
+            $(`a > small`, html).each(function () {
+                const name = $(this).text();
+                //const url = $(this).attr('href');
+                characters.push({
+                    name
+                })
+            })
+            res.json(characters);
+        }).catch((err) => console.log(err))
+
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
